3a5547ec117118953e08ac6286a978beea879b56
thepaven <thepaven@ffaaff6c-e182-11dd-a8eb-ddf07f3c1838>
2009-01-13 15:14:05 +0000

    First uppload from netbeans

---
+++ trunk/j2meClassesOnDesktop/src/javax/microedition/lcdui/Display.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javax.microedition.lcdui;
+
+import javax.microedition.lcdui.game.GameCanvas;
+import javax.microedition.midlet.MIDlet;
+
+
+
+/**
+ *
+ * @author Gustafsp
+ */
+public class Display {
+
+    public static Display getDisplay(MIDlet midlet) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void setCurrent(GameCanvas gameCanvas) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+}
---
+++ trunk/j2meClassesOnDesktop/src/javax/microedition/lcdui/game/GameCanvas.java
@@ -0,0 +1,54 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package javax.microedition.lcdui.game;
+
+import java.awt.Canvas;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+/**
+ *
+ * @author Gustafsp
+ */
+public class GameCanvas extends Canvas {
+    private GameCanvasHelper helper = new GameCanvasHelper();
+}
+ class GameCanvasHelper implements Runnable, MouseListener, MouseMotionListener {
+
+    public void run() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseClicked(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mousePressed(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseExited(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseDragged(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    public void mouseMoved(MouseEvent e) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+}
